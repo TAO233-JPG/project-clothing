0e360a3d288108cff5d29deb5dad497d481b6d87
TAO233-JPG <1040581451@qq.com>
2022-08-09 22:15:04 +0800

    feat: add categories-component and directory-component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from "../categoryItem/categoryItem.component";
+
+import './directory.style.scss'
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <CategoryItem category={category} key={category.id} />;
+      })}
+    </div>
+  );
+};
+
+export default Directory;
