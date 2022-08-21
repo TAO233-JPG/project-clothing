3ed380bfbe44528f28b67dd986af1145144d1170
TAO233-JPG <1040581451@qq.com>
2022-08-21 15:43:44 +0800

    feat: 引入reselect：引入reselect，修改selectCategoriesMap

--- src/store/categorise/categorise.select.js
+++ src/store/categorise/categorise.select.js
@@ -1,6 +1,21 @@
-export const selectCategoriesMap = ({ categories }) =>
-  categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+export const selectCategoriesReducer = (state) => state.categories;
+
+// 使用reselect进行缓存
+export const selectCategoriesMap = createSelector(
+  [selectCategoriesReducer],
+  (categories) =>
+    categories.categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
+
+// export const selectCategoriesMap = ({ categories }) =>
+//   categories.categories.reduce((acc, category) => {
+//     const { title, items } = category;
+//     acc[title.toLowerCase()] = items;
+//     return acc;
+//   }, {});
