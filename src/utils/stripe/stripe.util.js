202c05242d8ca74f64722b029c68febd0980a3f0
TAO233-JPG <1040581451@qq.com>
2022-09-02 22:12:49 +0800

    fix: 更改变量名REACT_APP_SECRET_PUBLISH_KEY =>  REACT_APP_STRIPE_PUBLISH_KEY

--- src/utils/stripe/stripe.util.js
+++ src/utils/stripe/stripe.util.js
@@ -1,3 +1,2 @@
 import { loadStripe } from "@stripe/stripe-js";
-console.log(process.env.REACT_APP_SECRET_PUBLISH_KEY);
-export const stripePromise = loadStripe(process.env.REACT_APP_SECRET_PUBLISH_KEY);
+export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISH_KEY);
